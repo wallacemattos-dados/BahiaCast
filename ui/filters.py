ff0c5c461999d0d794b385571f45d31afde4eefe
Irlan Wallace Dos Santos Mattos <99099948+wallacemattos-dados@users.noreply.github.com>
2026-01-30 14:22:15 -0300

    Create filters.py

---
+++ ui/filters.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def date_filter():
+    col1, col2 = st.columns(2)
+    with col1:
+        month = st.selectbox("Mês", range (1, 13))
+    with col2:
+        year = se.selectbox("Ano", range (1980, 2005))
+    return month, year
